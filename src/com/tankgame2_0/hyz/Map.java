56b021227fb31ed1ad9ae61dcee8aecdd8e2ca97
hyz951226 <hyz951226@gmail.com>
2018-09-18 17:33:37 +0800

    TankGame

---
+++ src/com/tankgame2_0/hyz/Map.java
@@ -0,0 +1,23 @@
+package com.tankgame2_0.hyz;
+/**
+ * 用于存放障碍物
+ * @author HYZ
+ *
+ */
+class Map {
+	/**
+	 * 
+	 */
+	Barrier [] bar = null;
+	public Map(){
+		
+	}
+	
+}
+class Barrier{
+	/**
+	 * 
+	 */
+	int x,y,width,height,type;
+	
+}
---
+++ src/com/tankgame2_0/hyz/TankGame2_0.java
@@ -0,0 +1,176 @@
+package com.tankgame2_0.hyz;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+/*
+ * 作者：胡有志
+ * 功能：实现坦克大战
+ * 日期：2018.6.7
+ * 版本：2.0
+ */
+public class TankGame2_0 extends JFrame implements Runnable{
+
+	
+	/**
+	 * 构建窗体，添加组件
+	 * 组件1，GamePanel
+	 * 组件2，ScorePanel--显示分数
+	 * 组件3，LifePanel--显示生命值
+	 * 组件4，MenuBar--菜单条,选择继续游戏或开始新游戏
+	 */
+	private static final long serialVersionUID = 2L;
+	
+	/**
+	 * welcome --欢迎，选择界面
+	 */
+	WelcomePanel welcomePanel = null;
+	/**
+	 * 
+	 */
+	StagePanel stagePanel = null;
+	/**
+	 * GamePanel--坦克运行界面
+	 */
+	GamePanel gamePanel=null;
+	/**
+	 * scorePanel --成绩界面，以及其中包含的成绩数据
+	 */
+	ScorePanel scorePanel = null;
+	Thread twp = null;
+	
+	Thread tstp = null;
+	
+	Thread tgp = null;
+	
+	//
+	Thread tsp =null;
+	
+	/**
+	 * 
+	 */
+	JLabel boundsL = null;
+	//
+	JLabel boundsU = null;
+	
+	JLabel boundsD = null;
+	/**
+	 * 
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+		
+		TankGame2_0 tankgame = new TankGame2_0();
+		Thread ttg = new Thread(tankgame);
+		ttg.start();
+	}
+	/**
+	 * 构造函数，新建组件，线程
+	 */
+	public TankGame2_0(){
+
+	}
+	public void addwelPanel(){
+		welcomePanel = new WelcomePanel();
+		
+		this.setResizable(false);
+		
+		this.add(welcomePanel);
+		this.addKeyListener(welcomePanel);
+		this.setTitle("Tank Game");
+		this.setSize(800,600);
+		setProperties();
+		
+		//新建并启动welcomePanel线程tgp。
+		twp = new Thread(welcomePanel);
+		twp.start();
+	}
+	
+	public void addstagePanel(){
+		
+		stagePanel = new StagePanel();
+		this.remove(welcomePanel);
+		this.welcomePanel = null;
+		this.add(stagePanel);
+		setProperties();
+		tstp = new Thread(stagePanel);
+		tstp.start();
+		//移除welcome面板
+		AePlayWave apw=new AePlayWave("voice/start.wav");
+		apw.start();
+	}
+	
+	public void addgasPanel(){
+		
+		gamePanel = new GamePanel();
+		
+		scorePanel = new ScorePanel();
+		
+		boundsL = new JLabel();
+		boundsL.setOpaque(true);
+		boundsL.setBackground(Color.gray);
+		boundsU = new JLabel();
+		boundsU.setOpaque(true);
+		boundsU.setBackground(Color.gray);
+		boundsD = new JLabel();
+		boundsD.setOpaque(true);
+		boundsD.setBackground(Color.gray);
+		//添加Action
+		this.addKeyListener(gamePanel);
+		//移除stagePanel面板
+		this.remove(this.stagePanel);
+		//
+		
+		this.add(boundsL,BorderLayout.WEST);
+		this.add(boundsU,BorderLayout.NORTH);
+		this.add(boundsD,BorderLayout.SOUTH);
+		this.add(gamePanel,BorderLayout.CENTER);
+		this.add(scorePanel,BorderLayout.EAST);
+		gamePanel.setPreferredSize(new Dimension(680,560));
+		scorePanel.setPreferredSize(new Dimension(100,600));
+		boundsL.setPreferredSize(new Dimension(20,600));
+		boundsU.setPreferredSize(new Dimension(500,20));
+		boundsD.setPreferredSize(new Dimension(500,20));
+		
+		//新建并启动gamePanel线程tgp。
+		tgp = new Thread(gamePanel);
+		tgp.start();
+		//
+		tsp = new Thread(scorePanel);
+		tsp.start();
+		
+		setProperties();
+	}
+	
+	public void setProperties(){
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		addwelPanel();
+		while(true){
+			if(gamePanel == null&&scorePanel == null){
+				if(!twp.isAlive()&&stagePanel == null ){
+					addstagePanel();
+				}else if(welcomePanel == null && !tstp.isAlive()){
+					addgasPanel();
+				}
+				
+			}
+			if(gamePanel != null&&scorePanel != null){
+				scorePanel.etNum = gamePanel.ets.size();
+				scorePanel.score = gamePanel.enSize-scorePanel.etNum;
+			}
+		}
+	}
+}
